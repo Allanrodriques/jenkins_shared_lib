10228ab5131abc9c9103e1057de5d7bce44066c2
AllanRodriques <allanrodriques2001@gmail.com>
2023-08-11 10:57:39 +0530

    updated build image to ECR

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,14 @@
-def call(String project,String ImageTag, String hubUser){
+// def call(String project,String ImageTag, String hubUser){
+//     sh """
+//     docker image build -t ${hubUser}/${project} .
+//     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// }
+
+def call(String awsaccount_id,String region, String ecr_repoName){
     sh """
-    docker image build -t ${hubUser}/${project} .
-    docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-    docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+        docker build -t ${ecr_repoName}
+        docker tag ${ecr_repoName}:latest ${awsaccount_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
