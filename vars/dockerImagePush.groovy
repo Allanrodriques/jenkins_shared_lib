3bf784d79c3e9d4fcec0df7669507499e5076429
AllanRodriques <allanrodriques2001@gmail.com>
2023-08-11 14:00:33 +0530

    updated

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -10,9 +10,6 @@
 // }
 // }
 def call(String awsaccount_id,String region, String ecr_repoName){
-    sh """
-    aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 473871105018.dkr.ecr.us-east-2.amazonaws.com
-    docker push ${ecr_repoName}:latest ${awsaccount_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
+    sh "aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 473871105018.dkr.ecr.us-east-2.amazonaws.com"
 }  
 
