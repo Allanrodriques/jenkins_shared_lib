f2ea4882369e56e6de95780bb11b583853b0d139
AllanRodriques <allanrodriques2001@gmail.com>
2023-08-11 11:12:21 +0530

    updated

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,6 +1,11 @@
-def call(String project,String ImageTag, String hubUser){
+// def call(String project,String ImageTag, String hubUser){
+//     sh """
+//     docker rmi ${hubUser}/${project}:${ImageTag}
+//     docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// } 
+def call(String awsaccount_id,String region, String ecr_repoName){
     sh """
-    docker rmi ${hubUser}/${project}:${ImageTag}
-    docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+        docker rmi ${ecr_repoName}:latest ${awsaccount_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
